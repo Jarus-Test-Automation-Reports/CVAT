1b04498d66a4d452803fa2f8d8c4d0001067cd9e
Jarus-Test-Automation-Reports <91470343+Jarus-Test-Automation-Reports@users.noreply.github.com>
2025-12-14 15:05:32 +0530

    Update FullAssessmentPdfService.cs

--- Services/PDF/FullAssessmentPdfService.cs
+++ Services/PDF/FullAssessmentPdfService.cs
@@ -1,5 +1,5 @@
 using CAT.AID.Models;
-using CAT.AID.Models.DTO;
+using CAT.AID.Web.Models.DTO;
 using QuestPDF.Fluent;
 using QuestPDF.Infrastructure;
 using QuestPDF.Helpers;
@@ -294,3 +294,4 @@ namespace CAT.AID.Web.Services.PDF
         }
     }
 }
+
